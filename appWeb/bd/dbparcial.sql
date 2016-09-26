79d5de30584c482a9fe4341ab013859ccf15f961
gitHAMP <hmendo81@gmail.com>
2016-09-26 07:45:55 -0500

    Script Base de Datos con Procedimientos Usuario

--- appWeb/bd/dbparcial.sql
+++ appWeb/bd/dbparcial.sql
@@ -18,8 +18,177 @@ USE `dbparcial`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `categoria`
+--
+
+DROP TABLE IF EXISTS `categoria`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `categoria` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(25) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `categoria`
+--
+
+LOCK TABLES `categoria` WRITE;
+/*!40000 ALTER TABLE `categoria` DISABLE KEYS */;
+INSERT INTO `categoria` VALUES (16,'Comedia'),(18,'Accion'),(21,'Terror');
+/*!40000 ALTER TABLE `categoria` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `item`
+--
+
+DROP TABLE IF EXISTS `item`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `item` (
+  `iditem` int(11) NOT NULL AUTO_INCREMENT,
+  `cantidad` int(11) DEFAULT NULL,
+  `importe` double DEFAULT NULL,
+  `idpelicula` int(11) DEFAULT NULL,
+  `idpedido` int(11) DEFAULT NULL,
+  PRIMARY KEY (`iditem`),
+  KEY `fk_item_pelicula_idx` (`idpelicula`),
+  KEY `fk_item_pedido_idx` (`idpedido`),
+  CONSTRAINT `fk_item_pedido` FOREIGN KEY (`idpedido`) REFERENCES `pedido` (`idpedido`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_item_pelicula` FOREIGN KEY (`idpelicula`) REFERENCES `pelicula` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=47 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `item`
+--
+
+LOCK TABLES `item` WRITE;
+/*!40000 ALTER TABLE `item` DISABLE KEYS */;
+/*!40000 ALTER TABLE `item` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `pedido`
+--
+
+DROP TABLE IF EXISTS `pedido`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `pedido` (
+  `idpedido` int(11) NOT NULL AUTO_INCREMENT,
+  `fecha` varchar(15) DEFAULT NULL,
+  `total` double DEFAULT NULL,
+  `idusuario` int(11) DEFAULT NULL,
+  PRIMARY KEY (`idpedido`),
+  KEY `fk_pedido_usuario_idx` (`idusuario`),
+  CONSTRAINT `fk_pedido_usuario` FOREIGN KEY (`idusuario`) REFERENCES `usuario` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pedido`
+--
+
+LOCK TABLES `pedido` WRITE;
+/*!40000 ALTER TABLE `pedido` DISABLE KEYS */;
+/*!40000 ALTER TABLE `pedido` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `pelicula`
+--
+
+DROP TABLE IF EXISTS `pelicula`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `pelicula` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(30) DEFAULT NULL,
+  `precio` double DEFAULT NULL,
+  `stock` int(11) DEFAULT NULL,
+  `imagen` varchar(45) DEFAULT NULL,
+  `idcategoria` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_pelicula_categoria_idx` (`idcategoria`),
+  CONSTRAINT `fk_pelicula_categoria` FOREIGN KEY (`idcategoria`) REFERENCES `categoria` (`id`) ON DELETE SET NULL ON UPDATE SET NULL
+) ENGINE=InnoDB AUTO_INCREMENT=44 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pelicula`
+--
+
+LOCK TABLES `pelicula` WRITE;
+/*!40000 ALTER TABLE `pelicula` DISABLE KEYS */;
+INSERT INTO `pelicula` VALUES (31,'Pelicula 1',20,30,'breaking-bad',NULL),(32,'Pelicula 2',30,40,'dexter',NULL),(33,'Pelicula 3',40,40,'family-guy',16),(34,'Pelicula 4',40,30,'friends',16),(35,'Pelicula 5',40,40,'gossip-girl',NULL),(36,'Pelicula 6',34,45,'homeland',16),(37,'Pelicula 7',30,40,'how-i-met-your-mother',NULL),(38,'breaking-bad',12,12,'how-i-met-your-mother',NULL),(39,'friends',12,12,'how-i-met-your-mother',16),(40,'hklh',12,12,'breaking-bad',NULL),(41,'hh',12,12,'breaking-bad',NULL),(42,'Pelicula 9',13,13,'breaking-bad',18),(43,'Pelicula 20',12,30,'how-i-met-your-mother',18);
+/*!40000 ALTER TABLE `pelicula` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuario`
+--
+
+DROP TABLE IF EXISTS `usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `usuario` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `apellidos` varchar(20) NOT NULL,
+  `nombres` varchar(20) NOT NULL,
+  `correo` varchar(30) NOT NULL,
+  `password` tinyblob NOT NULL,
+  `autorizacion` varchar(10) NOT NULL COMMENT 'ADMIN / USER',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `IDX_usuarios_2` (`apellidos`,`nombres`),
+  UNIQUE KEY `IDX_usuarios_3` (`correo`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuario`
+--
+
+LOCK TABLES `usuario` WRITE;
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+INSERT INTO `usuario` VALUES (4,'upc','upc','upc@upc.com','Ú>Â!,>>ÜXÙ¸fas','USER'),(5,'admin','admin','tuadmin@upc.com','Ûãa1Ð£nÑì&','ADMIN'),(6,'Mendoza Puerta','Henry A.','hmendo81@gmail.com','à.[|ýòááM\n=-','USER');
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Dumping routines for database 'dbparcial'
 --
+/*!50003 DROP PROCEDURE IF EXISTS `sp_autentica_usuario` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_autentica_usuario`(
+ p_correo varchar(30),
+ p_password varchar(15)
+)
+BEGIN
+
+  select id,apellidos,nombres,correo,AES_DECRYPT(password,'llave') as password,
+        autorizacion
+  from usuario 
+  where correo=p_correo
+  and AES_DECRYPT(password,'llave')=p_password;
+
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 /*!50003 DROP PROCEDURE IF EXISTS `sp_delete_categoria` */;
 /*!50003 SET @saved_cs_client      = @@character_set_client */ ;
 /*!50003 SET @saved_cs_results     = @@character_set_results */ ;
@@ -137,6 +306,41 @@ values(p_nombre,p_precio,p_stock,p_imagen,p_idcategoria);
 set p_estado='ok';
 commit;
 
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `sp_insert_usuario` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_insert_usuario`(
+ out p_estado varchar(200),
+ p_apellidos varchar(20),
+ p_nombre varchar(20),
+ p_correo varchar(30),
+ p_password varchar(15)
+)
+BEGIN
+  DECLARE EXIT handler for sqlexception,sqlwarning,not found
+  BEGIN
+    rollback;
+    set p_estado='error';
+  END;
+  start transaction;
+  set p_estado=null;
+  insert into usuario(apellidos,nombres,correo,password,autorizacion)
+  values(p_apellidos,p_nombre,p_correo,AES_ENCRYPT(p_password,'llave'),'USER');
+  set p_estado='ok';
+  commit;
 END ;;
 DELIMITER ;
 /*!50003 SET sql_mode              = @saved_sql_mode */ ;
@@ -296,4 +500,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-09-18 18:10:30
+-- Dump completed on 2016-09-26  7:45:27
